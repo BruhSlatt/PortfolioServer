7a8c531c0d95654f074706dbb171b289cc53ac79
Tony <tony.n.flpa@gmail.com>
2019-06-26 11:15:47 -0400

    gucci

--- index.js
+++ index.js
@@ -4,10 +4,27 @@ const app = express()
 
 // adding middleware // setting up template engine
 app.set("view engine","ejs"); 
+
+app.use(express.static('./public'));
  
-app.get('/projects', function (req, res) {
-  res.render('projects');
+app.get('/home', function (req, res) {
+  res.render('home');
 });
+app.get('/bucketList', function (req, res) {
+    res.render('bucketList');
+  });
+  app.get('/games', function (req, res) {
+    res.render('games');
+  });
+  app.get('/interests', function (req, res) {
+    res.render('interests');
+  });
+  app.get('/music', function (req, res) {
+    res.render('music');
+  });
+  app.get('/projects', function (req, res) {
+    res.render('projects');
+  });
  
 app.listen(3000, function(err){
     if (err)
